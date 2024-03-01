e6ab6574e6b7176fa13eb2723909bb730d5e3ea0
Imtiaj Sajin <imtiajsajin@gmail.com>
2024-03-02 01:43:19 +0600

    Update textAdder.h

--- Add Text/textAdder.h
+++ Add Text/textAdder.h
@@ -2,7 +2,59 @@
 #include <string>
 #include <string>
 #include<windows.h>
-void drawText(std::string text, int posX, int posY, float scale, int fontWeight, float colorR, float colorG, float colorB) {
+#include <cmath>
+
+
+
+///////////////
+std::string originalText = "asdf$!";
+std::string targetText = "Welcome!";
+float animationDuration = 5.0; 
+float morphSpeed = 0.0; 
+
+std::string calculateMorphedText(float time) {
+    float t = std::min(1.0f, std::max(0.0f, time / animationDuration)); // Normalize time between 0 and 1
+    std::string morphedText;
+
+    for (size_t i = 0; i < originalText.size() && i < targetText.size(); ++i) {
+        float c1 = originalText[i];
+        float c2 = targetText[i];
+        float morphedChar = c1 * (1.0 - t) + c2 * t;
+        morphedText += static_cast<char>(morphedChar);
+    }
+
+    return morphedText;
+}
+
+
+void morphedText(std::string text, int posX, int posY, float scale, int fontWeight, float colorR, float colorG, float colorB) {
+    targetText=text;
+
+    std::string morphedText = calculateMorphedText(morphSpeed);
+
+    glPushMatrix();
+    glTranslatef(posX, posY, 0); 
+    glScalef(scale, scale, scale);
+    glLineWidth(fontWeight); 
+    glColor3f(colorR, colorG, colorB); 
+
+    // Render each character of the morphed text
+    for (char &c : morphedText) {
+        glutStrokeCharacter(GLUT_STROKE_ROMAN, c); //GLUT's stroke font to render text
+    }
+
+    glPopMatrix();
+}
+
+/////
+
+// Function to animate the text
+
+
+
+//////
+
+void staticText(std::string text, int posX, int posY, float scale, int fontWeight, float colorR, float colorG, float colorB) {
     glPushMatrix();
     glTranslatef(posX, posY, 0);
     glScalef(scale, scale, scale);
@@ -10,9 +62,10 @@ void drawText(std::string text, int posX, int posY, float scale, int fontWeight,
     glColor3f(colorR, colorG, colorB);
 
     for (char &c : text) {
-        glutStrokeCharacter(GLUT_STROKE_ROMAN, c); 
+        glutStrokeCharacter(GLUT_STROKE_ROMAN, c);
     }
 
     glPopMatrix();
 }
 
+//////////////
